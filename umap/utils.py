0067a59df0d6d61e64844e5d2206e166dab0c036
Adam Gayoso <adamgayoso@users.noreply.github.com>
2020-01-04 15:39:50 -0800

    update make heap decorator

--- umap/utils.py
+++ umap/utils.py
@@ -130,7 +130,7 @@ def rejection_sample(n_samples, pool_size, rng_state):
     return result
 
 
-@numba.njit("f8[:, :, :](i8,i8)")
+@numba.njit()
 def make_heap(n_points, size):
     """Constructor for the numba enabled heap objects. The heaps are used
     for approximate nearest neighbor search, maintaining a list of potential
@@ -153,7 +153,7 @@ def make_heap(n_points, size):
     -------
     heap: An ndarray suitable for passing to other numba enabled heap functions.
     """
-    result = np.zeros((int(3), int(n_points), int(size)), dtype=np.float64)
+    result = np.zeros((3, int(n_points), int(size)), dtype=np.float64)
     result[0] = -1
     result[1] = np.infty
     result[2] = 0
